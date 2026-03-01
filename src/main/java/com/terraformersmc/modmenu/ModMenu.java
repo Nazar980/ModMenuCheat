b0b1147f97b6348f706505c14216dfefb751036c
Nazar980 <golovysiaut5@gmail.com>
2026-03-01 12:55:11 +0300

    Update ModMenu.java

--- src/main/java/com/terraformersmc/modmenu/ModMenu.java
+++ src/main/java/com/terraformersmc/modmenu/ModMenu.java
@@ -24,10 +24,10 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.fabricmc.loader.api.metadata.ModMetadata;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.resource.language.I18n;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,234 +37,238 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ModMenu implements ClientModInitializer {
-	public static final String MOD_ID = "modmenu";
-	public static final String GITHUB_REF = "TerraformersMC/ModMenu";
-	public static final Logger LOGGER = LoggerFactory.getLogger("Mod Menu");
-	public static final Gson GSON;
-	public static final Gson GSON_MINIFIED;
+    public static final String MOD_ID = "modmenu";
+    public static final String GITHUB_REF = "TerraformersMC/ModMenu";
+    public static final Logger LOGGER = LoggerFactory.getLogger("Mod Menu");
+    public static final Gson GSON;
+    public static final Gson GSON_MINIFIED;
 
-	static {
-		GsonBuilder builder = new GsonBuilder().registerTypeHierarchyAdapter(Enum.class, new EnumToLowerCaseJsonConverter()).setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
-		GSON = builder.setPrettyPrinting().create();
-		GSON_MINIFIED = builder.create();
-	}
+    static {
+        GsonBuilder builder = new GsonBuilder().registerTypeHierarchyAdapter(Enum.class, new EnumToLowerCaseJsonConverter()).setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+        GSON = builder.setPrettyPrinting().create();
+        GSON_MINIFIED = builder.create();
+    }
 
-	public static final Map<String, Mod> MODS = new ConcurrentHashMap<>();
-	public static final Map<String, Mod> ROOT_MODS = new ConcurrentHashMap<>();
-	public static final ListMultimap<Mod, Mod> PARENT_MAP = Multimaps.synchronizedListMultimap(LinkedListMultimap.create());
+    public static final Map<String, Mod> MODS = new ConcurrentHashMap<>();
+    public static final Map<String, Mod> ROOT_MODS = new ConcurrentHashMap<>();
+    public static final ListMultimap<Mod, Mod> PARENT_MAP = Multimaps.synchronizedListMultimap(LinkedListMultimap.create());
 
-	private static final Map<String, ConfigScreenFactory<?>> configScreenFactories = new ConcurrentHashMap<>();
-	private static final List<ModMenuApi> apiImplementations = new ArrayList<>();
+    private static final Map<String, ConfigScreenFactory<?>> configScreenFactories = new ConcurrentHashMap<>();
+    private static final List<ModMenuApi> apiImplementations = new ArrayList<>();
 
-	private static int cachedDisplayedModCount = -1;
-	public static final boolean RUNNING_QUILT = FabricLoader.getInstance().isModLoaded("quilt_loader");
-	public static final boolean DEV_ENVIRONMENT = FabricLoader.getInstance().isDevelopmentEnvironment();
-	public static final boolean TEXT_PLACEHOLDER_COMPAT = FabricLoader.getInstance().isModLoaded("placeholder-api");
+    private static int cachedDisplayedModCount = -1;
+    public static final boolean RUNNING_QUILT = FabricLoader.getInstance().isModLoaded("quilt_loader");
+    public static final boolean DEV_ENVIRONMENT = FabricLoader.getInstance().isDevelopmentEnvironment();
+    public static final boolean TEXT_PLACEHOLDER_COMPAT = FabricLoader.getInstance().isModLoaded("placeholder-api");
 
-	public static boolean hasConfigScreen(String modId) {
-		return getConfigScreenFactory(modId) != null;
-	}
+    public static boolean hasConfigScreen(String modId) {
+        return getConfigScreenFactory(modId) != null;
+    }
 
-	public static @Nullable Screen getConfigScreen(String modId, Screen parent) {
-		ConfigScreenFactory<?> factory = getConfigScreenFactory(modId);
-		if (factory != null) {
-			return factory.create(parent);
-		} else {
-			return null;
-		}
-	}
+    public static @Nullable Screen getConfigScreen(String modId, Screen parent) {
+        ConfigScreenFactory<?> factory = getConfigScreenFactory(modId);
+        if (factory != null) {
+            return factory.create(parent);
+        } else {
+            return null;
+        }
+    }
 
-	private static @Nullable ConfigScreenFactory<?> getConfigScreenFactory(String modId) {
-		if (ModMenuConfig.HIDDEN_CONFIGS.getValue().contains(modId)) {
-			return null;
-		}
+    private static @Nullable ConfigScreenFactory<?> getConfigScreenFactory(String modId) {
+        if (ModMenuConfig.HIDDEN_CONFIGS.getValue().contains(modId)) {
+            return null;
+        }
 
-		for (ModMenuApi api : apiImplementations) {
-			var factoryProviders = api.getProvidedConfigScreenFactories();
-			if (!factoryProviders.isEmpty()) {
-				factoryProviders.forEach(configScreenFactories::putIfAbsent);
-			}
-		}
+        for (ModMenuApi api : apiImplementations) {
+            var factoryProviders = api.getProvidedConfigScreenFactories();
+            if (!factoryProviders.isEmpty()) {
+                factoryProviders.forEach(configScreenFactories::putIfAbsent);
+            }
+        }
 
-		return configScreenFactories.get(modId);
-	}
+        return configScreenFactories.get(modId);
+    }
 
-	@Override
-	public void onInitializeClient() {
-		ModMenuConfigManager.initializeConfig();
-		Set<String> modpackMods = new HashSet<>();
-		Map<String, UpdateChecker> updateCheckers = new HashMap<>();
-		Map<String, UpdateChecker> providedUpdateCheckers = new HashMap<>();
+    @Override
+    public void onInitializeClient() {
+        ModMenuConfigManager.initializeConfig();
+        Set<String> modpackMods = new HashSet<>();
+        Map<String, UpdateChecker> updateCheckers = new HashMap<>();
+        Map<String, UpdateChecker> providedUpdateCheckers = new HashMap<>();
 
-		// Ignore deprecations, they're from Quilt Loader being in the dev env
-		//noinspection deprecation
-		FabricLoader.getInstance().getEntrypointContainers("modmenu", ModMenuApi.class).forEach(entrypoint -> {
-			//noinspection deprecation
-			ModMetadata metadata = entrypoint.getProvider().getMetadata();
-			String modId = metadata.getId();
-			try {
-				ModMenuApi api = entrypoint.getEntrypoint();
+        // Ignore deprecations, they're from Quilt Loader being in the dev env
+        //noinspection deprecation
+        FabricLoader.getInstance().getEntrypointContainers("modmenu", ModMenuApi.class).forEach(entrypoint -> {
+            //noinspection deprecation
+            ModMetadata metadata = entrypoint.getProvider().getMetadata();
+            String modId = metadata.getId();
+            try {
+                ModMenuApi api = entrypoint.getEntrypoint();
                 ConfigScreenFactory<?> factory = api.getModConfigScreenFactory();
                 if (!(factory instanceof NullScreenFactory<?>)) {
                     configScreenFactories.put(modId, factory);
                 }
-				apiImplementations.add(api);
-				updateCheckers.put(modId, api.getUpdateChecker());
-				providedUpdateCheckers.putAll(api.getProvidedUpdateCheckers());
-				api.attachModpackBadges(modpackMods::add);
-			} catch (Throwable e) {
-				LOGGER.error("Mod {} provides a broken implementation of ModMenuApi", modId, e);
-			}
-		});
+                apiImplementations.add(api);
+                updateCheckers.put(modId, api.getUpdateChecker());
+                providedUpdateCheckers.putAll(api.getProvidedUpdateCheckers());
+                api.attachModpackBadges(modpackMods::add);
+            } catch (Throwable e) {
+                LOGGER.error("Mod {} provides a broken implementation of ModMenuApi", modId, e);
+            }
+        });
 
-		// Fill mods map
-		//noinspection deprecation
-		for (ModContainer modContainer : FabricLoader.getInstance().getAllMods()) {
-			Mod mod;
-			if (RUNNING_QUILT) {
-				mod = new QuiltMod(modContainer, modpackMods);
-			} else {
-				mod = new FabricMod(modContainer, modpackMods);
-			}
+        // Fill mods map
+        //noinspection deprecation
+        for (ModContainer modContainer : FabricLoader.getInstance().getAllMods()) {
+            // --- ИЗМЕНЕНИЕ: Пропускаем CheatUtils ---
+            if (modContainer.getMetadata().getId().equalsIgnoreCase("CheatUtils")) {
+                continue;
+            }
+            // ----------------------------------------
 
-			var updateChecker = updateCheckers.get(mod.getId());
-			if (updateChecker == null) {
-				updateChecker = providedUpdateCheckers.get(mod.getId());
-			}
+            Mod mod;
+            if (RUNNING_QUILT) {
+                mod = new QuiltMod(modContainer, modpackMods);
+            } else {
+                mod = new FabricMod(modContainer, modpackMods);
+            }
 
-			MODS.put(mod.getId(), mod);
-			mod.setUpdateChecker(updateChecker);
-		}
+            var updateChecker = updateCheckers.get(mod.getId());
+            if (updateChecker == null) {
+                updateChecker = providedUpdateCheckers.get(mod.getId());
+            }
 
-		checkForUpdates();
+            MODS.put(mod.getId(), mod);
+            mod.setUpdateChecker(updateChecker);
+        }
 
-		Map<String, Mod> dummyParents = new HashMap<>();
+        checkForUpdates();
 
-		// Initialize parent map
-		HashSet<String> modParentSet = new HashSet<>();
-		for (Mod mod : MODS.values()) {
-			String parentId = mod.getParent();
-			if (parentId == null) {
-				ROOT_MODS.put(mod.getId(), mod);
-				continue;
-			}
+        Map<String, Mod> dummyParents = new HashMap<>();
 
-			Mod parent;
-			modParentSet.clear();
-			while (true) {
-				parent = MODS.getOrDefault(parentId, dummyParents.get(parentId));
-				if (parent == null) {
-					if (mod instanceof FabricMod) {
-						parent = new FabricDummyParentMod((FabricMod) mod, parentId);
-						dummyParents.put(parentId, parent);
-					}
-				}
+        // Initialize parent map
+        HashSet<String> modParentSet = new HashSet<>();
+        for (Mod mod : MODS.values()) {
+            String parentId = mod.getParent();
+            if (parentId == null) {
+                ROOT_MODS.put(mod.getId(), mod);
+                continue;
+            }
 
-				parentId = parent != null ? parent.getParent() : null;
-				if (parentId == null) {
-					// It will most likely end here in the first iteration
-					break;
-				}
+            Mod parent;
+            modParentSet.clear();
+            while (true) {
+                parent = MODS.getOrDefault(parentId, dummyParents.get(parentId));
+                if (parent == null) {
+                    if (mod instanceof FabricMod) {
+                        parent = new FabricDummyParentMod((FabricMod) mod, parentId);
+                        dummyParents.put(parentId, parent);
+                    }
+                }
 
-				if (modParentSet.contains(parentId)) {
-					LOGGER.warn("Mods contain each other as parents: {}", modParentSet);
-					parent = null;
-					break;
-				}
+                parentId = parent != null ? parent.getParent() : null;
+                if (parentId == null) {
+                    break;
+                }
 
-				modParentSet.add(parentId);
-			}
+                if (modParentSet.contains(parentId)) {
+                    LOGGER.warn("Mods contain each other as parents: {}", modParentSet);
+                    parent = null;
+                    break;
+                }
 
-			if (parent == null) {
-				ROOT_MODS.put(mod.getId(), mod);
-				continue;
-			}
+                modParentSet.add(parentId);
+            }
 
-			PARENT_MAP.put(parent, mod);
-		}
+            if (parent == null) {
+                ROOT_MODS.put(mod.getId(), mod);
+                continue;
+            }
 
-		MODS.putAll(dummyParents);
-		ModMenuEventHandler.register();
-	}
+            PARENT_MAP.put(parent, mod);
+        }
 
-	public static void clearModCountCache() {
-		cachedDisplayedModCount = -1;
-	}
+        MODS.putAll(dummyParents);
+        ModMenuEventHandler.register();
+    }
 
-	public static void checkForUpdates() {
-		UpdateCheckerUtil.checkForUpdates();
-	}
+    public static void clearModCountCache() {
+        cachedDisplayedModCount = -1;
+    }
 
-	public static boolean areModUpdatesAvailable() {
-		if (!ModMenuConfig.UPDATE_CHECKER.getValue()) {
-			return false;
-		}
+    public static void checkForUpdates() {
+        UpdateCheckerUtil.checkForUpdates();
+    }
 
-		for (Mod mod : MODS.values()) {
-			if (mod.isHidden()) {
-				continue;
-			}
+    public static boolean areModUpdatesAvailable() {
+        if (!ModMenuConfig.UPDATE_CHECKER.getValue()) {
+            return false;
+        }
 
-			if (!ModMenuConfig.SHOW_LIBRARIES.getValue() && mod.getBadges().contains(Mod.Badge.LIBRARY)) {
-				continue;
-			}
+        for (Mod mod : MODS.values()) {
+            if (mod.isHidden()) {
+                continue;
+            }
 
-			if (mod.hasUpdate() || mod.getChildHasUpdate()) {
-				return true; // At least one currently visible mod has an update
-			}
-		}
+            if (!ModMenuConfig.SHOW_LIBRARIES.getValue() && mod.getBadges().contains(Mod.Badge.LIBRARY)) {
+                continue;
+            }
 
-		return false;
-	}
+            if (mod.hasUpdate() || mod.getChildHasUpdate()) {
+                return true; 
+            }
+        }
 
-	public static String getDisplayedModCount() {
-		if (cachedDisplayedModCount == -1) {
-			boolean includeChildren = ModMenuConfig.COUNT_CHILDREN.getValue();
-			boolean includeLibraries = ModMenuConfig.COUNT_LIBRARIES.getValue();
-			boolean includeHidden = ModMenuConfig.COUNT_HIDDEN_MODS.getValue();
+        return false;
+    }
 
-			// listen, if you have >= 2^32 mods then that's on you
-			cachedDisplayedModCount = Math.toIntExact(MODS.values().stream().filter(mod -> {
-				boolean isChild = mod.getParent() != null;
-				if (!includeChildren && isChild) {
-					return false;
-				}
+    public static String getDisplayedModCount() {
+        if (cachedDisplayedModCount == -1) {
+            boolean includeChildren = ModMenuConfig.COUNT_CHILDREN.getValue();
+            boolean includeLibraries = ModMenuConfig.COUNT_LIBRARIES.getValue();
+            boolean includeHidden = ModMenuConfig.COUNT_HIDDEN_MODS.getValue();
 
-				boolean isLibrary = mod.getBadges().contains(Mod.Badge.LIBRARY);
-				if (!includeLibraries && isLibrary) {
-					return false;
-				}
+            cachedDisplayedModCount = Math.toIntExact(MODS.values().stream().filter(mod -> {
+                boolean isChild = mod.getParent() != null;
+                if (!includeChildren && isChild) {
+                    return false;
+                }
 
-				return includeHidden || !mod.isHidden();
-			}).count());
-		}
+                boolean isLibrary = mod.getBadges().contains(Mod.Badge.LIBRARY);
+                if (!includeLibraries && isLibrary) {
+                    return false;
+                }
 
-		return NumberFormat.getInstance().format(cachedDisplayedModCount);
-	}
+                return includeHidden || !mod.isHidden();
+            }).count());
+        }
 
-	public static Text createModsButtonText(boolean title) {
-		var titleStyle = ModMenuConfig.MODS_BUTTON_STYLE.getValue();
-		var gameMenuStyle = ModMenuConfig.GAME_MENU_BUTTON_STYLE.getValue();
-		var isIcon = title ?
-			titleStyle == ModMenuConfig.TitleMenuButtonStyle.ICON :
-			gameMenuStyle == ModMenuConfig.GameMenuButtonStyle.ICON;
-		var isShort = title ?
-			titleStyle == ModMenuConfig.TitleMenuButtonStyle.SHRINK :
-			gameMenuStyle == ModMenuConfig.GameMenuButtonStyle.REPLACE;
-		MutableText modsText = ModMenuScreenTexts.TITLE.copy();
-		if (ModMenuConfig.MOD_COUNT_LOCATION.getValue().isOnModsButton() && !isIcon) {
-			String count = ModMenu.getDisplayedModCount();
-			if (isShort) {
-				modsText.append(Text.literal(" ")).append(Text.translatable("modmenu.loaded.short", count));
-			} else {
-				String specificKey = "modmenu.loaded." + count;
-				String key = I18n.hasTranslation(specificKey) ? specificKey : "modmenu.loaded";
-				if (ModMenuConfig.EASTER_EGGS.getValue() && I18n.hasTranslation(specificKey + ".secret")) {
-					key = specificKey + ".secret";
-				}
-				modsText.append(Text.literal(" ")).append(Text.translatable(key, count));
-			}
-		}
-		return modsText;
-	}
+        return NumberFormat.getInstance().format(cachedDisplayedModCount);
+    }
+
+    public static Component createModsButtonText(boolean title) {
+        var titleStyle = ModMenuConfig.MODS_BUTTON_STYLE.getValue();
+        var gameMenuStyle = ModMenuConfig.GAME_MENU_BUTTON_STYLE.getValue();
+        var isIcon = title ?
+                titleStyle == ModMenuConfig.TitleMenuButtonStyle.ICON :
+                gameMenuStyle == ModMenuConfig.GameMenuButtonStyle.ICON;
+        var isShort = title ?
+                titleStyle == ModMenuConfig.TitleMenuButtonStyle.SHRINK :
+                gameMenuStyle == ModMenuConfig.GameMenuButtonStyle.REPLACE;
+        MutableComponent modsText = ModMenuScreenTexts.TITLE.copy();
+        if (ModMenuConfig.MOD_COUNT_LOCATION.getValue().isOnModsButton() && !isIcon) {
+            String count = ModMenu.getDisplayedModCount();
+            if (isShort) {
+                modsText.append(Component.literal(" ")).append(Component.translatable("modmenu.loaded.short", count));
+            } else {
+                String specificKey = "modmenu.loaded." + count;
+                String key = I18n.exists(specificKey) ? specificKey : "modmenu.loaded";
+                if (ModMenuConfig.EASTER_EGGS.getValue() && I18n.exists(specificKey + ".secret")) {
+                    key = specificKey + ".secret";
+                }
+                modsText.append(Component.literal(" ")).append(Component.translatable(key, count));
+            }
+        }
+        return modsText;
+    }
 }
